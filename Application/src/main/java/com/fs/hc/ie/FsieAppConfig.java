e635271c0884a1be9d7bc9c15a96c8a7f7cbb2df
fanyihui <faen_yh@163.com>
2021-04-26 17:17:59 +0800

    Initial commit

---
+++ Application/src/main/java/com/fs/hc/ie/FsieAppConfig.java
@@ -0,0 +1,31 @@
+package com.fs.hc.ie;
+
+import org.apache.camel.component.hl7.HL7MLLPCodec;
+import org.apache.camel.component.hl7.HL7MLLPNettyDecoderFactory;
+import org.apache.camel.component.hl7.HL7MLLPNettyEncoderFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FsieAppConfig {
+    @Bean
+    HL7MLLPNettyDecoderFactory hl7Decoder(){
+        HL7MLLPNettyDecoderFactory hl7MLLPNettyDecoderFactory = new HL7MLLPNettyDecoderFactory();
+        hl7MLLPNettyDecoderFactory.setConvertLFtoCR(true);
+        return hl7MLLPNettyDecoderFactory;
+    }
+
+    @Bean
+    HL7MLLPNettyEncoderFactory hl7Encoder(){
+        HL7MLLPNettyEncoderFactory hl7MLLPNettyEncoderFactory = new HL7MLLPNettyEncoderFactory();
+        hl7MLLPNettyEncoderFactory.setConvertLFtoCR(true);
+        return hl7MLLPNettyEncoderFactory;
+    }
+
+    @Bean
+    HL7MLLPCodec hl7MLLPCodec(){
+        HL7MLLPCodec hl7Codec = new HL7MLLPCodec();
+        hl7Codec.setConvertLFtoCR(true);
+        return hl7Codec;
+    }
+}
---
+++ Application/src/main/java/com/fs/hc/ie/routes/HL7v2RouteTest.java
@@ -0,0 +1,19 @@
+package com.fs.hc.ie.routes;
+
+import static org.apache.camel.component.hl7.HL7.messageConformsTo;
+import static org.apache.camel.component.hl7.HL7.ack;
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HL7v2RouteTest extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+        from("netty:tcp://localhost:8889?sync=true&encoders=#hl7Encoder&decoders=#hl7Decoder").
+                routeId("hl7v2test").
+                log("${body}").
+                unmarshal().hl7().
+                log("${body}").transform(ack());
+    }
+}
