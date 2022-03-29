b4dccc7999bd5870edd1482efec0a0676bebd7dd
fanyihui <faen_yh@163.com>
2022-03-29 20:43:56 +0800

    initial HIP

--- Application/src/main/java/com/fs/hc/ie/FsieAppConfig.java
+++ Application/src/main/java/com/fs/hc/ie/FsieAppConfig.java
@@ -1,8 +1,11 @@
 package com.fs.hc.ie;
 
+import org.apache.camel.component.cxf.CxfEndpoint;
 import org.apache.camel.component.hl7.HL7MLLPCodec;
 import org.apache.camel.component.hl7.HL7MLLPNettyDecoderFactory;
 import org.apache.camel.component.hl7.HL7MLLPNettyEncoderFactory;
+import org.hl7.v3.HIPMessageServerResponse;
+import org.hl7.v3.HL7MessageServerService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -27,6 +30,18 @@ public class FsieAppConfig {
         HL7MLLPCodec hl7Codec = new HL7MLLPCodec();
         hl7Codec.setConvertLFtoCR(true);
         return hl7Codec;
+    }
+
+    @Bean
+    CxfEndpoint chisEndpoint(){
+        CxfEndpoint cxfEndpoint = new CxfEndpoint();
+        cxfEndpoint.setServiceClass(HL7MessageServerService.class);
+        cxfEndpoint.setAddress("http://localhost:9001/hipMessageServer");
+        cxfEndpoint.setBeanId("chis2020");
+        cxfEndpoint.setEndpointName("CHIS2020");
+        cxfEndpoint.setWsdlURL("wsdl/HIPMessageServer.wsdl");
+
+        return cxfEndpoint;
     }
 
 }
